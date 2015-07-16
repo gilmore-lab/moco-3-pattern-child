9d8a210a5b7102938f9083ace74534041163ffaf
rogilmore <thatrickgilmore@gmail.com>
2015-07-16 12:01:25 -0400

    Bulk rmarkdown rendering

---
+++ moco-3-pattern-analysis.R
@@ -0,0 +1,13 @@
+# Renders adult and child analyses of moco-3-pattern study
+
+# Adult data
+rmarkdown::render('moco-3-pattern-adult-1F1.Rmd')
+rmarkdown::render('moco-3-pattern-adult-2F1.Rmd')
+rmarkdown::render('moco-3-pattern-adult-3F1.Rmd')
+rmarkdown::render('moco-3-pattern-adult-1F2.Rmd')
+
+# Child data
+rmarkdown::render('moco-3-pattern-child-1F1.Rmd')
+rmarkdown::render('moco-3-pattern-child-2F1.Rmd')
+rmarkdown::render('moco-3-pattern-child-3F1.Rmd')
+rmarkdown::render('moco-3-pattern-child-1F2.Rmd')
